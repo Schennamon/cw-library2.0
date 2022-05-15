968f89e19d1150ce52571185c89df113711fb61a
itolokneev <itolokneev.codica@gmail.com>
2022-05-15 17:23:24 +0300

    [Feature] Setup S3 imgae uploading

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -1,10 +1,15 @@
 ActiveAdmin.register Book do
-  permit_params :title, :country, :language, :description, :publication_date,
+  permit_params :title, :country, :language, :description, :publication_date, :cover,
                 book_genres_attributes: %i[id genre_id book_id _destroy]
 
   show do
     attributes_table do
       @book = Book.find_by(id: params[:id])
+      if @book.cover.attached?
+        row :cover do
+          image_tag @book.cover, width: "200px"
+        end
+      end
       row :title
       row :country
       row :language
@@ -35,6 +40,7 @@ ActiveAdmin.register Book do
       f.input :country
       f.input :language
       f.input :description
+      f.input :cover, as: :file
       f.has_many :book_genres, allow_destroy: true do |b|
         b.inputs 'Genres' do
           b.input :genre
